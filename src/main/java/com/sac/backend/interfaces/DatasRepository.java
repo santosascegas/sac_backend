ab57b86fd032c7ff95197e3315a5417669119994
mauricio <mauriciofreire_14@hotmail.com>
2021-06-03 20:29:32 -0300

    Lógica do endpoint de Datas

---
+++ src/main/java/com/sac/backend/interfaces/DatasRepository.java
@@ -0,0 +1,23 @@
+package com.sac.backend.interfaces;
+
+import com.sac.backend.models.DatasModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Maurício Freire
+ * Date 03/06/2021 at 18:31
+ * Created on IntelliJ IDEA
+ */
+public interface DatasRepository extends JpaRepository<DatasModel, Date> {
+
+    @Query("SELECT * FROM Datas WHERE ic_disponivel = true ORDER BY id")
+    List<DatasModel> findByDisponibilidade();
+
+    @Query("SELECT * FROM Datas WHERE datas BETWEEN CURRENT_DATE() "
+        + "+ CURRENT_DATE() + 7")
+    List<DatasModel> findBySemana();
+}
