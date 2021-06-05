d8eb99f4215dc19474b78d938bdf12b94a962d26
mauricio <mauriciofreire_14@hotmail.com>
2021-06-05 18:16:51 -0300

    Classe de testes para a entidade Datas.

--- src/test/java/com/sac/backend/BackendTeste.java
+++ src/test/java/com/sac/backend/BackendTeste.java
@@ -1,6 +1,11 @@
 package com.sac.backend;
 
+import com.sac.backend.DTO.FaleConoscoDTO;
+import com.sac.backend.helpers.EmailSender;
+import com.sac.backend.interfaces.AgendamentoRepository;
+import com.sac.backend.models.Agendamento;
 import com.sac.backend.models.Datas;
+import com.sac.backend.services.AgendamentoService;
 import com.sac.backend.services.DatasService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -26,6 +32,15 @@ public class BackendTeste {
     
     @Autowired
     DatasService datasService;
+    
+    @Autowired
+    EmailSender emailSender;
+    
+    @Autowired
+    AgendamentoService agendamentoService;
+    
+    @Autowired
+    AgendamentoRepository agendamentoRepository;
     
     @Test
     public void salvarDataPasseio() {
@@ -85,7 +100,75 @@ public class BackendTeste {
         datasService.create(data3);
         List<Datas> base = Arrays.asList(data1, data3);
         List<Datas> datas = datasService.listarPorStatus();
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < datas.size(); i++)
             assertSame(base.get(i).getId(), datas.get(i).getId());
+    }
+    
+    @Test
+    public void enviarEmailConfirmacao() {
+        FaleConoscoDTO faleConosco = new FaleConoscoDTO();
+        faleConosco.setEmail("mauriciofreire@unisantos.br");
+        assertTrue(emailSender.enviarEmailFaleConosco(faleConosco),
+                "Falha ao enviar o email.");
+    }
+    
+    @Test
+    public void salvarAgendamentoPasseio() {
+        Agendamento agendamento = new Agendamento(1L, "João Silva", "jsilva@gmail.com",
+                "55.184.332-5", "99121473", 0);
+        Datas datas = agendamento.getData();
+        datas.setStatus(1);
+        datasService.update(datas);
+        assertEquals(1, datasService.findById(datas.getId()).get().getStatus());
+        assertTrue(agendamentoService.create(agendamento) instanceof Agendamento);
+    }
+    
+    @Test
+    public void encontrarAgendamentoPorId() {
+        agendamentoRepository.save(new Agendamento(1L, "João Silva",
+                "jsilva@gmail.com", "55.184.332-5", "99121473", 0));
+        Optional<Agendamento> agendamento = agendamentoService.findById(1L);
+        assertNotNull(agendamento.get().getId(), "Agendamento nao encontrado.");
+    }
+    
+    @Test
+    public void encontrarTodosAgendamentos() {
+        Agendamento[] agendamentosArray = { new Agendamento(),
+                new Agendamento(), new Agendamento() };
+        String nome[] = { "Ana", "João", "Mario" };
+        for (int id = 1; id < agendamentosArray.length; id++) {
+            agendamentosArray[id].setId((long) id);
+            agendamentosArray[id].setNomeUsuario(nome[id]);
+            agendamentosArray[id].setEmailUsuario(nome[id].concat("@gmail.com"));
+            agendamentosArray[id].setDocumento("11.222.333-" + id);
+            agendamentosArray[id].setTelefone("999666321");
+            agendamentosArray[id].setAtestado(0);
+            agendamentoRepository.save(agendamentosArray[id]);
+        }
+        List<Agendamento> agendamentos = agendamentoService.findAll();
+        for (Agendamento agendamento : agendamentos)
+            assertTrue(agendamento instanceof Agendamento);
+    }
+    
+    @Test
+    public void atualizarAgendamento() {
+        agendamentoRepository.save(new Agendamento(1L, "João Silva",
+                "jsilva@gmail.com", "55.184.332-5", "99121473", 0));
+        Optional<Agendamento> agendamento = agendamentoService.findById(1L);
+        if (agendamento.isPresent())
+            assertTrue(agendamentoService.delete(agendamento.get().getId()),
+                    "Registro não disponível para exclusão.");
+        else assertNotNull(agendamento, "Registro não encontrado.");
+    }
+    
+    @Test
+    public void excluirAgendamento() {
+        agendamentoRepository.save(new Agendamento(1L, "João Silva",
+                "jsilva@gmail.com","55.184.332-5", "99121473", 0));
+        Optional<Agendamento> agendamento = agendamentoService.findById(1L);
+        if (agendamento.isPresent())
+            assertTrue(agendamentoService.delete(agendamento.get().getId()),
+                    "Registro nao disponivel para exclusão.");
+        else assertNotNull(agendamento, "Registro não encontrado.");
     }
 }
