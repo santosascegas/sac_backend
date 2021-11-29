586567bc99a09e41a5942b85a2670aa0711ace85
marcellosso <marcel.losso@gmail.com>
2021-11-29 12:58:01 -0300

    Fixed java version

--- banco/sac_banco_0906.sql
+++ banco/sac_banco_0906.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `agendamento`
 --
 
+use nshlz4k4t661ae0k;
+
 DROP TABLE IF EXISTS `agendamento`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
