5834784192d36417b49cd446b4a834633c4f058c
mauricio <mauriciofreire_14@hotmail.com>
2021-12-10 23:26:52 -0300

    Classe de testes para a entidade Datas.

--- src/test/java/com/sac/backend/BackendTeste.java
+++ src/test/java/com/sac/backend/BackendTeste.java
@@ -9,18 +9,21 @@ import com.sac.backend.models.Datas;
 import com.sac.backend.services.AgendamentoService;
 import com.sac.backend.services.DatasService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -47,9 +50,11 @@ public class BackendTeste {
     @Autowired
     AgendamentoRepository agendamentoRepository;
     
+    static Integer N = 30;
+    
     @Test
     public void salvarDataPasseio() {
-        Datas datas = new Datas(1L, "07/12/2021", 0);
+        Datas datas = new Datas(1L, "08/12/2021", 0);
         assertAll(() -> assertNotNull(datas.getId(), "O id não deve ser nulo."),
                 () -> assertNotNull(datas.getData(), "A data não deve ser nula."),
                 () -> assertTrue(datas.getStatus() >= 0 && datas.getStatus() <= 1,
@@ -59,40 +64,37 @@ public class BackendTeste {
         );
     }
     
-    @Test
-    public void procurarData() {
-        datasRepository.save(new Datas(1L, "07/12/2021", 0));
-        Optional<Datas> datas = datasService.findById(28L);
-        assertNotNull(datas.get(), "O id não foi encontrado.");
+    @ParameterizedTest
+    @MethodSource(value = "aleatorio")
+    public void procurarData(long id) {
+        Optional<Datas> datas = datasService.findById(id);
+        assertTrue(datas.isPresent(), "Nenhuma data encontrada.");
     }
     
     @Test
     public void procurarTodasAsDatas() {
         List<Datas> datasList = datasService.findAll();
         for (Datas d: datasList)
-            assertTrue(d instanceof Datas, "Registro incorreto.");
+            assertAll("Datas", () -> assertNotNull(d.getData(), "Data nula."),
+                    () -> assertTrue(d instanceof Datas, "Registro incompatível."));
     }
     
-    @Test
-    public void atualizarDatas() {
-        double r = Math.random() * 30;
-        long rand = (long) (r + 1);
-        
-        Optional<Datas> atualizacao = datasService.findById(rand);
+    @ParameterizedTest
+    @MethodSource(value = "aleatorio")
+    public void atualizarDatas(long id) {
+        Optional<Datas> atualizacao = datasService.findById(id);
         atualizacao.ifPresent(value -> assertNotEquals(1, value.getStatus()));
         atualizacao.ifPresent(value -> value.setStatus(1));
-        assertAll("Atualização",
-                () -> assertNotNull(atualizacao.get(), "Registro não encontrado."),
+        assertAll("Atualização de Data",
+                () -> assertTrue(atualizacao.isPresent(), "Registro não encontrado."),
                 () -> assertTrue(datasService.update(atualizacao.get()), "Falhou")
         );
     }
     
-    @Test
-    public void deletarDatas() {
-        double r = Math.random() * 30;
-        long rand = (long) (r + 1);
-        System.out.println("-->> " + rand);
-        Optional<Datas> datas = datasService.findById(12L);
+    @ParameterizedTest
+    @MethodSource(value = "aleatorio")
+    public void deletarDatas(long id) {
+        Optional<Datas> datas = datasService.findById(id);
         assertAll("Deletar", () -> assertTrue(datas.isPresent(), "Não presente."),
                 () -> assertTrue(datasService.delete(datas.get().getId()),
                         "A data não foi deletada."));
@@ -101,9 +103,8 @@ public class BackendTeste {
     @Test
     public void listarDatasPorStatus() {
         List<Datas> datas = datasService.listarPorStatus();
-        System.out.println("LISTA " + datas.size());
         for (Datas d : datas)
-            assertSame(0, d.getStatus());
+            assertSame(0, d.getStatus(), "Status inesperado!");
     }
     
     @Test
@@ -114,70 +115,77 @@ public class BackendTeste {
                 "Falha ao enviar o email.");
     }
     
-    @Test
-    public void salvarAgendamentoPasseio() {
-        Agendamento agendamento = new Agendamento(1L, "Urubutre",
-                "urubutre@gmail.com", "42.000.352-1", "99066293", 1);
-        double r = Math.random() * 27;
-        long rand = (long) (r + 1);
-        Optional<Datas> optional = datasService.findById(rand);
+    @ParameterizedTest
+    @MethodSource(value = "aleatorio")
+    public void salvarAgendamentoPasseio(long id) {
+        Agendamento agendamento = new Agendamento(1L, "Max Verstappen",
+                "supermax@gmail.com", "33.333.333-3", "993233033", 1);
+        Optional<Datas> optional = datasService.findById(id);
         Datas datas = optional.get();
-        if (optional.isPresent()) {
-            assertEquals(0, datas.getStatus(), "Data já agendada.");
-            datas.setStatus(1);
-            datasService.update(datas);
-            agendamento.setData(optional.get());
-        }
-        assertEquals(1, datas.getStatus());
-        assertTrue(agendamentoRepository.save(agendamento) instanceof Agendamento);
+        if (!optional.isPresent())
+            assertThrows(NullPointerException.class,
+                    () -> System.out.println("Nenhum registro encontrado."));
+
+        assertEquals(0, datas.getStatus(), "Data já agendada.");
+        datas.setStatus(1);
+        datasService.update(datas);
+        agendamento.setData(optional.get());
+        
+        assertEquals(1, datas.getStatus(), "Status da data não atualizado!");
+        assertAll("Verificar criação da data",
+                () -> assertNotNull(agendamento.getNomeUsuario()),
+                () -> assertNotNull(agendamento.getEmailUsuario()),
+                () -> assertNotNull(agendamento.getData()),
+                () -> assertNotNull(agendamento.getDocumento()),
+                () -> assertNotNull(agendamento.getTelefone()),
+                () -> assertNotNull(agendamento.getAtestado()),
+                () -> assertTrue(agendamentoService.create(agendamento) instanceof Agendamento));
     }
     
-    @Test
-    public void encontrarAgendamentoPorId() {
-        long rand = (long) (Math.random() * (24 - 15) + 15);
-        Optional<Agendamento> agendamento = agendamentoService.findById(rand);
-        System.out.println("ID = " + rand + "\nagendamento: " + agendamento.get().getNomeUsuario());
-        assertNotNull(agendamento, "Agendamento nao encontrado.");
+    @ParameterizedTest
+    @MethodSource(value = "aleatorio")
+    public void encontrarAgendamentoPorId(long id) {
+        Optional<Agendamento> agendamento = agendamentoService.findById(id);
+        agendamento.ifPresent( usuario -> System.out.println(usuario.getNomeUsuario()));
+        agendamento.ifPresentOrElse(value ->
+                        assertNotNull(agendamento), new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("Nenhum agendamento foi " +
+                                "encontradado com ID = " + id + "!");
+                    }
+                });
     }
     
     @Test
     public void encontrarTodosAgendamentos() {
-        Agendamento[] agendamentosArray = { new Agendamento(),
-                new Agendamento(), new Agendamento() };
-        String nome[] = { "Ana", "João", "Mario" };
-        for (int id = 1; id < agendamentosArray.length; id++) {
-            agendamentosArray[id].setId((long) id);
-            agendamentosArray[id].setNomeUsuario(nome[id]);
-            agendamentosArray[id].setEmailUsuario(nome[id].concat("@gmail.com"));
-            agendamentosArray[id].setDocumento("11.222.333-" + id);
-            agendamentosArray[id].setTelefone("999666321");
-            agendamentosArray[id].setAtestado(0);
-            agendamentoRepository.save(agendamentosArray[id]);
-        }
         List<Agendamento> agendamentos = agendamentoService.findAll();
-        for (Agendamento agendamento : agendamentos)
-            assertTrue(agendamento instanceof Agendamento);
+        for (Agendamento agendamento : agendamentos) {
+            assertAll("FindAll", () -> assertNotNull(agendamento),
+                    () -> assertNotNull(agendamento.getData(),
+                            agendamento.getId() + " sem data."),
+                    () -> assertTrue(agendamento instanceof Agendamento));
+        }
     }
     
-    @Test
-    public void atualizarAgendamento() {
-        agendamentoRepository.save(new Agendamento(1L, "João Silva",
-                "jsilva@gmail.com", "55.184.332-5", "99121473", 0));
-        Optional<Agendamento> agendamento = agendamentoService.findById(1L);
-        if (agendamento.isPresent())
-            assertTrue(agendamentoService.delete(agendamento.get().getId()),
-                    "Registro não disponível para exclusão.");
-        else assertNotNull(agendamento, "Registro não encontrado.");
+    @ParameterizedTest
+    @MethodSource(value = "aleatorio")
+    public void atualizarAgendamento(long id) {
+        Optional<Agendamento> agendamento = agendamentoService.findById(id);
+        assertTrue(agendamento.isPresent(), "Registro não encontrado!");
+        assertTrue(agendamentoService.update(agendamento.get()));
     }
     
-    @Test
-    public void excluirAgendamento() {
-        agendamentoRepository.save(new Agendamento(1L, "João Silva",
-                "jsilva@gmail.com","55.184.332-5", "99121473", 0));
-        Optional<Agendamento> agendamento = agendamentoService.findById(1L);
-        if (agendamento.isPresent())
-            assertTrue(agendamentoService.delete(agendamento.get().getId()),
-                    "Registro nao disponivel para exclusão.");
-        else assertNotNull(agendamento, "Registro não encontrado.");
+    @ParameterizedTest
+    @MethodSource(value = "aleatorio")
+    public void excluirAgendamento(long id) {
+        Optional<Agendamento> agendamento = agendamentoService.findById(id);
+        assertNotNull(agendamento.get(), "Registro não encontrado!");
+        assertTrue(agendamentoService.delete(agendamento.get().getId()));
+    }
+    
+    public static Stream<Long> aleatorio() {
+        long rand = (long) (Math.random() * N + 1);
+        return Stream.of(rand);
     }
 }
